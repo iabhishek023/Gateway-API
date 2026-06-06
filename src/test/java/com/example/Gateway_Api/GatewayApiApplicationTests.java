cdcd33f7df12fb53907bbea17380ef66792ccfed
Abhishek <iabhishek023@gmail.com>
2026-06-06 11:21:23 +0530

    Files added

---
+++ src/test/java/com/example/Gateway_Api/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gateway_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
